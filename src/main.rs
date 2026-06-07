eb4f4a169d885ecf946daaec3d05e7d240f48c8a
thetrueryan <georgytoper@gmail.com>
2026-06-07 17:03:11 +0400

    add cli commands parsing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,32 @@ mod lenovo;
 mod info;
 mod battery;
 
+use clap::Parser;
+use cli::Cli;
+
+use crate::info::print_info;
+use crate::cli::{Commands, ConservationAction};
+use crate::lenovo::conservation::set_conservation;
+
 
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Info => {
+            print_info();
+        }
+
+        Commands::Conservation { action } => {
+            match action {
+                ConservationAction::On => {
+                    set_conservation(true).unwrap();
+                }
+
+                ConservationAction::Off => {
+                    set_conservation(false).unwrap();
+                }
+            }
+        }
+    }
 }
